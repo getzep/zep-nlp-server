6003c00b38f3b7c44d59325dca9e5956a8046792
Daniel Chalef <131175+danielchalef@users.noreply.github.com>
2023-05-22 13:54:36 -0700

    locust load test

---
+++ load_test.py
@@ -0,0 +1,26 @@
+from locust import HttpUser, task, between
+import uuid
+import json
+
+
+class ApiUser(HttpUser):
+    wait_time = between(1, 2.5)
+
+    @task
+    def post_entities(self):
+        headers = {'Content-Type': 'application/json'}
+        data = {
+            "texts": [
+                {
+                    "uuid": str(uuid.uuid4()),
+                    "text": """South Korea’s Kospi gained as much as 1%, on track for its sixth 
+    daily advance. Samsung Electronics Co. and SK Hynix Inc. were among the biggest 
+    contributors to the benchmark after China said their US rival Micron Technology 
+    Inc. had failed to pass a cybersecurity review. "I think you’re gonna see that 
+    begin to thaw very shortly,” between the US and China, Biden said on Sunday 
+    after a Group-of-Seven summit in Japan. He added that his administration was 
+    considering whether to lift sanctions on Chinese Defense Minister Li Shangfu."""
+                }
+            ]
+        }
+        self.client.post("/entities", data=json.dumps(data), headers=headers)
