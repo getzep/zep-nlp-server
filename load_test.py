9e124252ceccfaf1b5350a7326a2127beb6d6c1b
Daniel Chalef <131175+danielchalef@users.noreply.github.com>
2023-06-15 13:48:55 -0700

    load test - format

--- load_test.py
+++ load_test.py
@@ -3,7 +3,8 @@ import uuid
 
 from locust import HttpUser, between, task
 
-text = """
+text = (
+    """
     South Korea’s Kospi gained as much as 1%, on track for its sixth 
     daily advance. Samsung Electronics Co. and SK Hynix Inc. were among the biggest 
     contributors to the benchmark after China said their US rival Micron Technology 
@@ -11,6 +12,8 @@ text = """
     begin to thaw very shortly,” between the US and China, Biden said on Sunday 
     after a Group-of-Seven summit in Japan. He added that his administration was 
     considering whether to lift sanctions on Chinese Defense Minister Li Shangfu.""",
+)
+
 
 class ApiUser(HttpUser):
     wait_time = between(1, 2.5)
@@ -18,24 +21,11 @@ class ApiUser(HttpUser):
     @task
     def post_entities(self):
         headers = {"Content-Type": "application/json"}
-        data = {
-            "texts": [
-                {
-                    "uuid": str(uuid.uuid4()),
-                    "text": text
-                }
-            ]
-        }
+        data = {"texts": [{"uuid": str(uuid.uuid4()), "text": text}]}
         self.client.post("/entities", data=json.dumps(data), headers=headers)
 
     @task
     def post_embeddings(self):
         headers = {"Content-Type": "application/json"}
-        data = {
-            "documents": [
-                {
-                    "text": text
-                }
-            ]
-        }
+        data = {"documents": [{"text": text}]}
         self.client.post("/embeddings", data=json.dumps(data), headers=headers)
