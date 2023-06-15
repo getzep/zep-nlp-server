ea121ceabcdd66922eed67c6cfddc443d5a63019
Daniel Chalef <131175+danielchalef@users.noreply.github.com>
2023-06-15 13:47:42 -0700

    load test

--- load_test.py
+++ load_test.py
@@ -26,3 +26,22 @@ class ApiUser(HttpUser):
             ]
         }
         self.client.post("/entities", data=json.dumps(data), headers=headers)
+
+    @task
+    def post_embeddings(self):
+        headers = {"Content-Type": "application/json"}
+        data = {
+            "documents": [
+                {
+                    "text": """
+    South Korea’s Kospi gained as much as 1%, on track for its sixth 
+    daily advance. Samsung Electronics Co. and SK Hynix Inc. were among the biggest 
+    contributors to the benchmark after China said their US rival Micron Technology 
+    Inc. had failed to pass a cybersecurity review. "I think you’re gonna see that 
+    begin to thaw very shortly,” between the US and China, Biden said on Sunday 
+    after a Group-of-Seven summit in Japan. He added that his administration was 
+    considering whether to lift sanctions on Chinese Defense Minister Li Shangfu.""",
+                }
+            ]
+        }
+        self.client.post("/embeddings", data=json.dumps(data), headers=headers)
