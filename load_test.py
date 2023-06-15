0a990844d7227cb3f651787ea77351d51e66cdc7
Daniel Chalef <131175+danielchalef@users.noreply.github.com>
2023-06-15 13:48:39 -0700

    load test

--- load_test.py
+++ load_test.py
@@ -3,6 +3,14 @@ import uuid
 
 from locust import HttpUser, between, task
 
+text = """
+    South Korea’s Kospi gained as much as 1%, on track for its sixth 
+    daily advance. Samsung Electronics Co. and SK Hynix Inc. were among the biggest 
+    contributors to the benchmark after China said their US rival Micron Technology 
+    Inc. had failed to pass a cybersecurity review. "I think you’re gonna see that 
+    begin to thaw very shortly,” between the US and China, Biden said on Sunday 
+    after a Group-of-Seven summit in Japan. He added that his administration was 
+    considering whether to lift sanctions on Chinese Defense Minister Li Shangfu.""",
 
 class ApiUser(HttpUser):
     wait_time = between(1, 2.5)
@@ -14,14 +22,7 @@ class ApiUser(HttpUser):
             "texts": [
                 {
                     "uuid": str(uuid.uuid4()),
-                    "text": """
-    South Korea’s Kospi gained as much as 1%, on track for its sixth 
-    daily advance. Samsung Electronics Co. and SK Hynix Inc. were among the biggest 
-    contributors to the benchmark after China said their US rival Micron Technology 
-    Inc. had failed to pass a cybersecurity review. "I think you’re gonna see that 
-    begin to thaw very shortly,” between the US and China, Biden said on Sunday 
-    after a Group-of-Seven summit in Japan. He added that his administration was 
-    considering whether to lift sanctions on Chinese Defense Minister Li Shangfu.""",
+                    "text": text
                 }
             ]
         }
@@ -33,14 +34,7 @@ class ApiUser(HttpUser):
         data = {
             "documents": [
                 {
-                    "text": """
-    South Korea’s Kospi gained as much as 1%, on track for its sixth 
-    daily advance. Samsung Electronics Co. and SK Hynix Inc. were among the biggest 
-    contributors to the benchmark after China said their US rival Micron Technology 
-    Inc. had failed to pass a cybersecurity review. "I think you’re gonna see that 
-    begin to thaw very shortly,” between the US and China, Biden said on Sunday 
-    after a Group-of-Seven summit in Japan. He added that his administration was 
-    considering whether to lift sanctions on Chinese Defense Minister Li Shangfu.""",
+                    "text": text
                 }
             ]
         }
